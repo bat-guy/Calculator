3e09ff5bfc9221923e2b322db94522e39b6d2645
netzwelt_dheeraj <dheeraj.parashar@vtnetzwelt.com>
2017-12-08 19:44:34 +0530

    -Code refactoring.

--- app/src/main/java/com/example/dheeraj/calculator/Calculator.java
+++ app/src/main/java/com/example/dheeraj/calculator/Calculator.java
@@ -6,28 +6,42 @@ package com.example.dheeraj.calculator;
 
 public class Calculator {
 
-    public static double add(double first, double second) {
-        return first + second;
-    }
-
-    public static double subtract(double first, double second) {
-        return first - second;
-    }
-
+    /**
+     * Method to multiple two numbers
+     * @param first first number
+     * @param second second number
+     * @return result
+     */
     public static double multiply(double first, double second) {
         return first * second;
     }
 
+    /**
+     * Method to divide two numbers
+     * @param first first number
+     * @param second second number
+     * @return result
+     */
     public static double divide(double first, double second) {
         return first / second;
     }
 
+    /**
+     * Method to calculate the power of a number
+     * @param number the number
+     * @param power the power of the number
+     * @return result
+     */
     public static double exponent(double number, double power) {
-
         return Math.pow(number, power);
-
     }
 
+    /**
+     * Method to get modulus of two numbers
+     * @param first first number
+     * @param second second number
+     * @return result
+     */
     public static double modulus(double first, double second) {
         return first % second;
     }
